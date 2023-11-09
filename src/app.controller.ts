65122ed7301752c879e483d165ab57739356b9f5
Yuriy Kravchenko <yuriy777kravchenko@gmail.com>
2023-11-09 16:22:23 +0200

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from "express";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Req() req: Request): string {
+    const protocol = req.protocol;
+    const host = req.get("Host");
+    const originUrl = req.originalUrl;
+    const fullUrl = `${protocol}//${host}//${originUrl}`;
+    return this.appService.getHello(fullUrl);
   }
 }
